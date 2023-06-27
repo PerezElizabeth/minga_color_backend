a91ff7be7016a8caa7c2a904e943a494754c3d32
Elizabeth Perez <129786245+PerezElizabeth@users.noreply.github.com>
2023-06-26 21:27:17 -0500

    authors+chapters+mangas+users (#6)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import author_router from './authors.js'
+import category_router from './categories.js'
+import manga_router from './mangas.js'
+import chapter_router from './chapters.js'
+import auth_router from './users.js'
 let router = express.Router();
 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/authors',author_router)        //obligo al enrutador principal a usar las rutas de authors con la palabrita /authors
+router.use('/categories',category_router)  //obligo al enrutador principal a usar las rutas de authors con la palabrita /categories
+router.use('/mangas',manga_router)        //obligo al enrutador principal a usar las rutas de authors con la palabrita /mangas
+router.use('/chapters',chapter_router)   //obligo al enrutador principal a usar las rutas de authors con la palabrita /chapters
+router.use('/auth',auth_router)         //obligo al enrutador principal a usar las rutas de authors con la palabrita /auth
 
 export default router
