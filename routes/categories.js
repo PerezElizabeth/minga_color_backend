c209c69e9ef7c0cf6c04347a958bbb55989f3dae
Elizabeth Perez <129786245+PerezElizabeth@users.noreply.github.com>
2023-06-26 21:43:10 -0500

    categories (#7)

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 import { Router} from "express"
+import read from '../controllers/categories/read.js'
 
 const category_router = Router()
 
 //category_router.post()   //POST: para crear un autor
-//category_router.get()    //GET: para leer (TODOS o SOLO UNO) autores
+category_router.get('/',read)    //GET: para leer (TODOS o SOLO UNO) autores
 //category_router.put()    //PUT: para actualizar un autor
 //category_router.delete() //DELETE: para eliminar un autor
 
